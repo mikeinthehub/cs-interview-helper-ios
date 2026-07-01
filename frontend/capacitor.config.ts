07e0e4b9be91e45755a22a99df23621f1d86c2c6
mikeinthehub <90553112+mikeinthehub@users.noreply.github.com>
2026-07-01 15:16:41 +0800

    Add frontend/capacitor.config.ts

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.csinterview.helper',
+  appName: 'CS面试助手',
+  webDir: '../backend/static'
+};
+
+export default config;
