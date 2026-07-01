485fb2a9c1b7a81cfa5056b629babdb5744ae96c
mikeinthehub <90553112+mikeinthehub@users.noreply.github.com>
2026-07-01 16:33:02 +0800

    [self-contained] frontend/capacitor.config.ts

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.csinterview.helper',
   appName: 'CS面试助手',
-  webDir: '../backend/static'
+  webDir: 'dist',
+  ios: {
+    contentInset: 'always',
+    scheme: 'CSInterviewHelper',
+    preferredContentMode: 'mobile',
+  },
 };
 
 export default config;
